46cd73c3d5407f955596ba1a64f5932e76894e10
hazem amaar <62269304+hazemamaar@users.noreply.github.com>
2023-03-19 17:46:27 +0200

    bluetooth logic

--- sag/src/main/java/com/example/sagapp/ble/ui/BluetoothActivity.kt
+++ sag/src/main/java/com/example/sagapp/ble/ui/BluetoothActivity.kt
@@ -9,14 +9,20 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.core.base.android.BaseActivity
 import com.example.core.extentions.observe
 import com.example.sagapp.databinding.ActivityBluetoothBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class BluetoothActivity : BaseActivity<ActivityBluetoothBinding,BluetoothViewModel>() {
 
+    @Inject
+    lateinit var scanDevicesAdapter: BluetoothListAdapter
+    @Inject
+    lateinit var pairedDevicesAdapter: BluetoothListAdapter
     private val bluetoothManager by lazy {
         applicationContext.getSystemService(BluetoothManager::class.java)
     }
@@ -59,9 +65,26 @@ class BluetoothActivity : BaseActivity<ActivityBluetoothBinding,BluetoothViewMod
                 )
             )
         }
-        mViewModel.observeDevices()
-        mViewModel.startScan()
+        binding.startScan.setOnClickListener {
+            mViewModel.startScan()
+            mViewModel.observeDevices()
+
+        }
+        binding.stopScan.setOnClickListener {
+            mViewModel.stopScan()
+        }
+        binding.startServer.setOnClickListener {
+            mViewModel.waitForIncomingConnections()
+        }
+        pairedDevicesAdapter.setOnItemClickListener {
+            mViewModel.connectToDevice(it)
+        }
+        scanDevicesAdapter.setOnItemClickListener {
+            mViewModel.connectToDevice(it)
+        }
         subscribeToObservers()
+        mViewModel.observeDevices()
+
     }
     private fun subscribeToObservers() {
         mViewModel.apply {
@@ -74,13 +97,24 @@ class BluetoothActivity : BaseActivity<ActivityBluetoothBinding,BluetoothViewMod
     private fun handleUiState(action: BluetoothAction) {
        when(action){
            is BluetoothAction.PairedDevices -> {
+               binding.scanRv.apply {
+                   pairedDevicesAdapter.bleDevices = action.pairedDevices
+                   adapter = pairedDevicesAdapter
+                   layoutManager = LinearLayoutManager(context)
+               }
+
                Toast.makeText(applicationContext, ""+action.pairedDevices, Toast.LENGTH_SHORT).show()
            }
            is BluetoothAction.ScannedDevices -> {
+               binding.pairedRv.apply {
+                   scanDevicesAdapter.bleDevices = action.scannedDevices
+                   adapter = scanDevicesAdapter
+                   layoutManager = LinearLayoutManager(context)
+               }
                Toast.makeText(applicationContext, ""+action.scannedDevices, Toast.LENGTH_SHORT).show()
            }
            is BluetoothAction.Connected -> {
-               Toast.makeText(applicationContext, ""+action.isConnected, Toast.LENGTH_SHORT).show()
+               Toast.makeText(applicationContext, "connecting sone"+action.isConnected, Toast.LENGTH_SHORT).show()
            }
            is BluetoothAction.Connecting -> {
                Toast.makeText(applicationContext, ""+action.isConnecting, Toast.LENGTH_SHORT).show()
