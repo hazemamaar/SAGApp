5c5c02df8ea055e11e3fd5d452d3487b469a8f34
aya215-g <ayag39480@gmail.com>
2023-05-25 00:48:06 +0300

    home screen

--- sag/src/main/java/com/example/sagapp/screens/ui/activities/screensActivity.kt
+++ sag/src/main/java/com/example/sagapp/screens/ui/activities/screensActivity.kt
@@ -1,12 +1,29 @@
 package com.example.sagapp.screens.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.sagapp.R
+import com.example.sagapp.authentication.ui.viewmodel.LoginViewModel
+import com.example.sagapp.screens.ui.fragments.homeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class screensActivity : AppCompatActivity() {
+
+    private val mViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screens)
+        actionBar?.hide()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+       bottomNavigationView.setupWithNavController(navController)
+
+
     }
 }
